23858f84c1433d1ed5f3a8f7ac8477d7a449d76d
SupraJames <james@pattinson.org>
2015-11-04 16:31:45 +0000

    remove unused crap from main loop

--- tilda-mavlink.ino
+++ tilda-mavlink.ino
@@ -19,34 +19,7 @@ void setup() {
 }
 
 void loop() {
-
-    //int sysid = 20;                   ///< ID 20 for this airplane
-    //int compid = MAV_COMP_ID_IMU;     ///< The component sending the message is the IMU, it could be also a Linux process
-    //int type = MAV_TYPE_QUADROTOR;   ///< This system is an airplane / fixed wing
- 
-// Define the system type, in this case an airplane
-   // uint8_t system_type = MAV_TYPE_FIXED_WING;
-   // uint8_t autopilot_type = MAV_AUTOPILOT_GENERIC;
- 
-  //  uint8_t system_mode = MAV_MODE_PREFLIGHT; ///< Booting up
-  //  uint32_t custom_mode = 0;                 ///< Custom mode, can be defined by user/adopter
-  //  uint8_t system_state = MAV_STATE_STANDBY; ///< System ready for flight
-    // Initialize the required buffers
-  //  mavlink_message_t msg;
-  //  uint8_t buf[MAVLINK_MAX_PACKET_LEN];
- 
-// Pack the message
-   // mavlink_msg_heartbeat_pack(sysid,compid, &msg, type, autopilot_type, system_mode, custom_mode, system_state);
- 
-// Copy the message to the send buffer
-  //  uint16_t len = mavlink_msg_to_send_buffer(buf, &msg);
- 
-// Send the message with the standard UART send function
-// uart0_send might be named differently depending on
-// the individual microcontroller / library in use.
-     //delay(1000);
-    // radioDongle.write(buf, len);
-     comm_receive();
+  comm_receive();
 }
 
 void comm_receive() {
